aab181a087e2915887c47caa868264e5196f9d27
Sebastianford <60185922+Sebastianford@users.noreply.github.com>
2020-04-10 15:05:31 -0400

    test

---
+++ Assets/ball.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ball : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "fail")
+        {
+            Destroy(this.gameObject);
+            SceneManager.LoadScene("Death");
+        }
+
+        if (other.tag == "goal")
+        {
+            Destroy(other.gameObject);
+            SceneManager.LoadScene("Victory");
+        }
+    }
+
+}
---
+++ Assets/ball2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ball2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "fail")
+        {
+            Destroy(this.gameObject);
+            SceneManager.LoadScene("dea2");
+        }
+
+        if (other.tag == "goal")
+        {
+            Destroy(other.gameObject);
+            SceneManager.LoadScene("Vict2");
+        }
+    }
+
+}
